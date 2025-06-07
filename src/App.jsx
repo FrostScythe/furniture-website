037bfa69bcb6bedde2a0b2567d0cc8f524c86fa2
Ayush Maheshwari <111050424+FrostScythe@users.noreply.github.com>
2025-06-07 11:40:42 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 // src/App.jsx
-import {FeatureCategory, HeroSection, NewArrivalSection,Nav} from './components';
+import {FeatureCategory, HeroSection, NewArrivalSection,Nav, Footer} from './components';
 
 function App() {
     return (
         <>
             <Nav/>
             <div>
+            <br/><br/>
                 <HeroSection/>
+                <br/>
+                <FeatureCategory/>
+                 <br/><br/><br/>
                 <NewArrivalSection/>
                 <br/><br/>
-                <FeatureCategory/>
+                <br/><br/>
+               <Footer/>
+                <br/><br/>
             </div>
         </>
     );
